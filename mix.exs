377d176baa5909032876086b9133d30b58370d44
José Valim <jose.valim@dashbit.co>
2020-04-07 18:24:54 +0200

    Support back to Elixir v1.6

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NimbleOptions.MixProject do
     [
       app: :nimble_options,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
