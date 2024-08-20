cc1bd8161d000e7430390cb09510687ae445e6ef
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-08-20 16:09:18 -0400

    Added test verifying message is redacted when using `NimbleOptions` API

--- test/nimble_options/validation_error_test.exs
+++ test/nimble_options/validation_error_test.exs
@@ -1,6 +1,8 @@
 defmodule NimbleOptions.ValidationErrorTest do
   use ExUnit.Case, async: true
 
+  alias NimbleOptions.ValidationError
+
   test "does not redact value when redact option is false" do
     schema = [foo: [type: :integer]]
 
@@ -19,5 +21,15 @@ defmodule NimbleOptions.ValidationErrorTest do
     {:error, error} = NimbleOptions.validate(opts, schema)
 
     assert inspect(error) =~ "value: \"**redacted**\""
+  end
+
+  test "message is redacted when an error is raised" do
+    schema = [foo: [type: :integer, redact: true]]
+
+    opts = [foo: "not an integer"]
+
+    assert_raise(ValidationError, "invalid value for :foo option: expected integer", fn ->
+      NimbleOptions.validate!(opts, schema)
+    end)
   end
 end
